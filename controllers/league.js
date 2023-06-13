91e1a4ecf094f34413eff323a2239e1765f66ea0
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2023-06-13 18:10:31 +0300

    added join leagues

--- controllers/league.js
+++ controllers/league.js
@@ -62,9 +62,24 @@ exports.myLeagues = async (req, res) => {
         })
     );
 
+    let leaguePlayers = [];
+    await Promise.all(
+        (leaguePlayers = await UserLeagueModel.findAll({
+            where: { league_id: user[0].dataValues.userLeagues[0]?.league.dataValues.id },
+            include: [
+                {
+                    model: UserModel,
+                    attributes: ["id", "nickName", "image"],
+                    as: "User",
+                },
+            ],
+        }))
+    );
+
     return res.status(200).json({
         message: "Leagues found",
         user,
+        leaguePlayers,
     });
 };
 
@@ -78,7 +93,7 @@ exports.createLeague = async (req, res) => {
     const { leagueName, userId } = req.body;
 
     try {
-        const newLeague = await Leaguemodel.create({
+        const newLeague = await LeagueModel.create({
             league_name: leagueName,
             league_image: req.file?.path ?? "leagueAvatars/league.jpg",
             admin_id: userId,
@@ -95,4 +110,63 @@ exports.createLeague = async (req, res) => {
         console.error("Error during create league:", err);
         res.status(500).json({ message: "Internal server error." });
     }
+};
+
+exports.joinLeague = async (req, res) => {
+    const { leagueNumber, userId } = req.params;
+    const league = await LeagueModel.findOne({
+        where: { league_number: leagueNumber },
+    });
+    if (!league) {
+        return res.status(404).json({ message: "League number not found" });
+    }
+
+    if (league.dataValues.admin_id == userId) {
+        return res.status(400).json({ message: "You are already the admin of this league" });
+    }
+
+    const isUSerAlreadyInLeague = await UserLeagueModel.findOne({
+        where: { user_id: userId, league_id: league.id },
+    });
+
+    if (isUSerAlreadyInLeague) {
+        return res.status(400).json({ message: "You are already in this league" });
+    }
+
+    const userLeague = await UserLeagueModel.create({
+        user_id: parseInt(userId),
+        league_id: parseInt(league.id),
+        is_admin: false,
+    });
+
+    return res.status(200).json({ message: "League joined", userLeague });
+
+    // const user = await UserModel.findOne({
+    //     where: { id: userId },
+    //     attributes: ["id", "nickName", "image"],
+    //     include: [
+    //         {
+    //             model: UserLeagueModel,
+    //             as: "userLeagues",
+    //             include: [
+    //                 {
+    //                     model: LeagueModel,
+    //                     as: "league",
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // });
+
+    // await Promise.all(
+    //     user.dataValues.userLeagues.map(async (userLeague) => {
+    //         const admin = await UserModel.findOne({
+    //             attributes: ["id", "nickName", "image"],
+    //             where: { id: userLeague.league?.dataValues.admin_id },
+    //         });
+    //         userLeague.league.dataValues.leagueAdmin = admin;
+    //     })
+    // );
+
+    // return res.status(200).json({ message: "League joined", userLeague, user });
 };
--- routes/leagues.js
+++ routes/leagues.js
@@ -8,4 +8,6 @@ router.get("/myLeagues/:userId", auth, leagueController.myLeagues);
 
 router.post("/createLeague", auth, upload.single("image"), leagueController.createLeague);
 
+router.put("/joinLeague/:leagueNumber/:userId", auth, leagueController.joinLeague);
+
 module.exports = router;
