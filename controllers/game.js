3b082e6c65fc771c384c62201fd9441dc0fe8f77
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2023-06-21 21:26:50 +0300

    added user to tserGames

--- controllers/game.js
+++ controllers/game.js
@@ -1,5 +1,3 @@
-// const init = require("../models/init");
-
 const GameModel = require("../models/Game");
 const UserGameModel = require("../models/UserGame");
 const GameDetailsModel = require("../models/GameDetails");
@@ -14,7 +12,7 @@ exports.newGame = async (req, res) => {
             isOpen: true,
         });
 
-        const usersGame = [];
+        const usersGames = [];
         const GameDetails = [];
 
         await Promise.all(
@@ -24,7 +22,24 @@ exports.newGame = async (req, res) => {
                     game_id: game.id,
                     league_id: selectedLeague[0]?.league_id,
                 });
-                usersGame.push(userGame);
+
+                //get users data for userGame
+                const userGameUser = await UserGameModel.findOne({
+                    where: {
+                        user_id: player,
+                        game_id: game.id,
+                    },
+                    // attributes: ["id", "user_id", "game_id", "league_id"],
+                    include: [
+                        {
+                            model: UserModel,
+                            as: "User",
+                            attributes: ["id", "nickName", "image"],
+                        },
+                    ],
+                });
+
+                usersGames.push(userGameUser);
 
                 const newGameDetails = await GameDetailsModel.create({
                     game_id: game.id,
@@ -53,8 +68,7 @@ exports.newGame = async (req, res) => {
                 GameDetails.push(newGameDetails);
             })
         );
-
-        return res.status(200).json({ message: `Game number ${game.id} created`, game, usersGame, GameDetails });
+        return res.status(200).json({ message: `Game number ${game.id} created`, game, usersGames, GameDetails });
     } catch (error) {
         console.error("Error creating game:", error);
     }
