3b0b3dae17f3ea544efd56c346576bddc8eb6f57
Nadav Galili <nadavg@mobile-brain.net>
2024-07-24 14:12:02 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,13 +5,14 @@ module.exports = (req, res, next) => {
     const token = req.header("x-auth-token");
     console.log("🚀 ~ token:", token)
     if (!token) return res.status(401).send("Access denied. No token provided.");
-    
+
     try {
         const decoded = jwt.verify(token, defaults.jwtKey);
         console.log("🚀 ~ decoded:", decoded)
         req.user = decoded;
         next();
     } catch (ex) {
+        console.log("🚀 ~ ex:", ex)
         res.status(400).send("Invalid token.");
     }
 };
