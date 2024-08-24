cc040d3d773cf6bf7ce9aec0ea7aa6f35c1b6ef0
Nadav Galili <nadavg@mobile-brain.net>
2024-08-24 22:03:31 +0300

    fixed user streKS

--- utils/statsUtils.js
+++ utils/statsUtils.js
@@ -70,3 +70,89 @@ exports.calculateWinnLossRatio = async (playerId, leagueId) => {
   const ratio = (winns / (winns + loss)) * 100;
   return ratio.toFixed(2);
 };
+
+exports.calculateUserStreaks = (games) => {
+  const users = {};
+
+  games.forEach((game) => {
+    const userId = game.user_id;
+    const profit = parseFloat(game?.dataValues?.total_profit);
+
+    if (!users[userId]) {
+      users[userId] = {
+        nickName: game.User.nickName,
+        image: game.User.image,
+        currentStreak: 0,
+        maxStreak: 0,
+        lastProfit: null,
+        totalGames: 0,
+        profitableGames: 0,
+      };
+    }
+
+    users[userId].totalGames++;
+    if (profit > 0) {
+      users[userId].profitableGames++;
+      if (users[userId].lastProfit === null || users[userId].lastProfit <= 0) {
+        // Start of a new streak
+        users[userId].currentStreak = 1;
+      } else {
+        // Continuing the streak
+        users[userId].currentStreak++;
+      }
+      users[userId].maxStreak = Math.max(
+        users[userId].maxStreak,
+        users[userId].currentStreak
+      );
+    } else {
+      // End of streak
+      users[userId].currentStreak = 0;
+    }
+
+    users[userId].lastProfit = profit;
+  });
+
+  return Object.entries(users)
+    .map(([userId, data]) => ({
+      id: userId,
+      nickName: data.nickName,
+      image: data.image,
+      title: data.maxStreak,
+      subTitle: data.currentStreak,
+      subTitle2: calculateWinLossRatio(data.profitableGames, data.totalGames),
+    }))
+    .sort((a, b) => b.title - a.title);
+};
+
+// Helper function to calculate and format the win-loss ratio
+const calculateWinLossRatio = (profitableGames, totalGames) => {
+  if (totalGames === 0) return "0.00%";
+  const ratio = (profitableGames / totalGames) * 100;
+  return ratio.toFixed(2);
+};
+
+exports.personalUserStreak = (games) => {
+  let currentStreak = 0;
+  let maxStreak = 0;
+  let userId = null;
+
+  for (const game of games) {
+    userId = game.user_id;
+    const profit = parseFloat(game.total_profit);
+
+    if (profit > 0) {
+      currentStreak++;
+      maxStreak = Math.max(maxStreak, currentStreak);
+    } else {
+      currentStreak = 0;
+    }
+  }
+
+  return [
+    {
+      id: parseInt(userId),
+      title: maxStreak.toString(),
+      subTitle: currentStreak.toString(),
+    },
+  ];
+};
